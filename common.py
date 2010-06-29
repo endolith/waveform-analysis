f770afa7f8badef047eb79473d23112f8498394a
Endolith <endolith@gmail.com>
2010-06-28 23:40:09 -0400

    move thdn readme into main readme

--- common.py
+++ common.py
@@ -19,11 +19,11 @@ def load(filename):
 
 def analyze_channels(filename, function):
     """Given a filename, run the given analyzer function on each channel of the file
-	
-	"""
+    
+    """
     signal, sample_rate, channels = load(filename)
     print 'Analyzing "' + filename + '"...'
-
+    
     if channels == 1:
         # Monaural
         function(signal, sample_rate)
@@ -64,21 +64,21 @@ def dB(level):
 def parabolic(f, x):
     """Quadratic interpolation for estimating the true position of an
     inter-sample maximum when nearby samples are known.
-   
+    
     f is a vector and x is an index for that vector.
-   
+    
     Returns (vx, vy), the coordinates of the vertex of a parabola that goes
     through point x and its two neighbors.
-   
+    
     Example:
     Defining a vector f with a local maximum at index 3 (= 6), find local
     maximum if points 2, 3, and 4 actually defined a parabola.
-   
+    
     In [3]: f = [2, 3, 1, 6, 4, 2, 3, 1]
-   
+    
     In [4]: parabolic(f, argmax(f))
     Out[4]: (3.2142857142857144, 6.1607142857142856)
-   
+    
     """
     # Requires real division.  Insert float() somewhere to force it?
     xv = 1/2 * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
