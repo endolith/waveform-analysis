b69242c07cf2a573cd341c43bd283236c0197209
Endolith <endolith@gmail.com>
2016-03-23 22:35:47 -0400

    docstrings

--- A_weighting.py
+++ A_weighting.py
@@ -12,6 +12,10 @@ http://www.mathworks.com/matlabcentral/fileexchange/69
 http://replaygain.hydrogenaudio.org/mfiles/adsgn.m
 
 Translated from adsgn.m to SciPy 2009-07-14 endolith@gmail.com
+
+When importing a stereo sound file with scikits.audiolab or pysoundfile, it
+needs axis = 0:
+y = lfilter(b, a, x, axis = 0)
 """
 
 from __future__ import division
@@ -23,10 +27,27 @@ def A_weighting(fs):
     """
     Design of an A-weighting filter.
     
-    B, A = A_weighting(fs) designs a digital A-weighting filter for
-    sampling frequency fs. Usage: y = lfilter(b, a, x).
+    Designs a digital A-weighting filter for
+    sampling frequency `fs`. Usage: y = lfilter(b, a, x).
     Warning: fs should normally be higher than 20 kHz. For example,
     fs = 48000 yields a class 1-compliant filter.
+
+    Since this uses the bilinear transform, frequency response around fs/2 will
+    be inaccurate at lower sampling rates.  A-weighting is undefined above
+    20 kHz, though.
+
+    Example:
+
+    from scipy.signal import freqz
+    import matplotlib.pyplot as plt
+    fs = 200000  # change to 48000 to see truncation
+    b, a = A_weighting(fs)
+    f = np.logspace(np.log10(10), np.log10(fs/2), 1000)
+    w = 2*pi * f / fs
+    w, h = freqz(b, a, w)
+    plt.semilogx(w*fs/(2*pi), 20*np.log10(abs(h)))
+    plt.grid(True, color='0.7', linestyle='-', which='both', axis='both')
+    plt.axis([10, 100e3, -50, 20])
 
     References:
        [1] IEC/CD 1672: Electroacoustics-Sound Level Meters, Nov. 1996.
@@ -64,6 +85,3 @@ def A_weight(signal, fs):
 
     b, a = A_weighting(fs)
     return lfilter(b, a, signal)
-
-# When importing a stereo sound file with scikits.audiolab, it needs axis = 0:
-# y = lfilter(B, A, x, axis = 0)
