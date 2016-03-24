a8ebf469716fe7cf7610210c1d6a807881b50ce7
Endolith <endolith@gmail.com>
2016-03-24 19:13:36 -0400

    PEP8 spacing

--- wave_analyzer.py
+++ wave_analyzer.py
@@ -37,6 +37,7 @@ def histogram(signal):
         hist(signal) #TODO: parameters, abs(signal)?
         show()
 
+
 def properties(signal, sample_rate):
     """
     Return a list of some wave properties for a given 1-D signal
@@ -45,21 +46,21 @@ def properties(signal, sample_rate):
     DC_offset = mean(signal)
     # Maximum/minimum sample value
     # Estimate of true bit rate
-    
+
     # Remove DC component
     signal -= mean(signal)
-    
+
     # Measurements that don't include DC
     signal_level = rms_flat(signal)
     peak_level = max(absolute(signal))
     crest_factor = peak_level/signal_level
-    
+
     # Apply the A-weighting filter to the signal
     weighted = A_weight(signal, sample_rate)
     weighted_level = rms_flat(weighted)
-    
+
     # TODO: rjust instead of tabs
-    
+
     return [
     'DC offset:\t%f (%.3f%%)' % (DC_offset, DC_offset * 100),
     'Crest factor:\t%.3f (%.3f dB)' % (crest_factor, dB(crest_factor)),
@@ -68,14 +69,15 @@ def properties(signal, sample_rate):
     'RMS A-weighted:\t%.3f (%.3f dBFS(A), %.3f dB)' % (weighted_level, dB(weighted_level), dB(weighted_level/signal_level)),
     '-----------------',
     ]
-    
+
+
 def analyze(filename):
     wave_file = Sndfile(filename, 'r')
     signal = wave_file.read_frames(wave_file.nframes)
     channels = wave_file.channels
     sample_rate = wave_file.samplerate
     header = 'dBFS values are relative to a full-scale square wave'
-    
+
     results = [
     'Properties for "' + filename + '"',
     str(wave_file.format),
@@ -93,22 +95,22 @@ def analyze(filename):
         results += properties(signal, sample_rate)
     elif channels == 2:
         # Stereo
-        if array_equal(signal[:,0],signal[:,1]):
+        if array_equal(signal[:, 0], signal[:, 1]):
             results += ['Left and Right channels are identical:']
-            results += properties(signal[:,0], sample_rate)
+            results += properties(signal[:, 0], sample_rate)
         else:
             results += ['Left channel:']
-            results += properties(signal[:,0], sample_rate)
+            results += properties(signal[:, 0], sample_rate)
             results += ['Right channel:']
-            results += properties(signal[:,1], sample_rate)
+            results += properties(signal[:, 1], sample_rate)
     else:
         # Multi-channel
         for ch_no, channel in enumerate(signal.transpose()):
             results += ['Channel %d:' % (ch_no + 1)]
             results += properties(channel, sample_rate)
-    
+
     display(header, results)
-    
+
     plot_histogram = False
     if plot_histogram:
         histogram(signal)
