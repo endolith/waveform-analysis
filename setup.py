131546240239d64c121fde06577e3827f15c8976
Endolith <endolith@gmail.com>
2017-07-05 17:20:11 -0400

    Add packaging and CI files
    Minimal setup.py
    
    CircleCI file (no idea if it works or not)
    
    Exclude private functions from Coverage
    (not sure if a good idea)

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='Waveform analysis',
+      version='0.1',
+      description='A collection of functions and scripts for analyzing '
+                  'waveforms, especially audio',
+      url='https://github.com/endolith/waveform_analysis',
+      author_email='endolith@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
