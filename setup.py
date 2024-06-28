ab0c275d32e5e01a1cb9e8b495608678d44e579f
endolith <endolith@gmail.com>
2024-06-27 22:03:59 -0400

    Change repository and distribution name to use hyphens

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import setup, find_packages
 
-setup(name='waveform_analysis',
+setup(name='waveform-analysis',
       version='0.1',
       description='A collection of functions and scripts for analyzing '
                   'waveforms, especially audio',
-      url='https://github.com/endolith/waveform_analysis',
+      url='https://github.com/endolith/waveform-analysis',
       author_email='endolith@gmail.com',
       license='MIT',
       packages=find_packages(),
