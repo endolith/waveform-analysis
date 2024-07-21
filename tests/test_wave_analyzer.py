1e67f3233cd5c0b657bcec82c6d317d0e197e125
endolith <endolith@gmail.com>
2024-07-21 00:05:16 -0400

    wave_analyzer is only a command-line script
    So test it only on the command line, not importable.

--- tests/test_wave_analyzer.py
+++ tests/test_wave_analyzer.py
@@ -5,26 +5,6 @@ import sys
 import pytest
 
 
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
-
-from scripts.wave_analyzer import wave_analyzer
-
-
-def test_wave_analyzer_no_args(capsys):
-    with pytest.raises(SystemExit) as excinfo:
-        wave_analyzer([])
-    assert "You must provide at least one file to analyze:" in str(
-        excinfo.value)
-    assert "python wave_analyzer.py filename.wav" in str(excinfo.value)
-
-
-def test_wave_analyzer_nonexistent_file(capsys):
-    wave_analyzer(["nonexistent.wav"])
-    captured = capsys.readouterr()
-    assert "Couldn't analyze \"nonexistent.wav\"" in captured.out
-
-
 def test_wave_analyzer_command_line():
     script_path = os.path.join(os.path.dirname(
         __file__), '..', 'scripts', 'wave_analyzer.py')
