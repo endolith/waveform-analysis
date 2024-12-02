baece1e4db3fa2324090086efe1d74cce314e65b
endolith <endolith@gmail.com>
2024-12-02 14:47:27 -0500

    Create working THD analyzer script
    Mimic the structure of the "measure frequency" script.  Previously the
    THD functions just printed their results, but now that they return
    values that isn't going to work.
    
    Clarify that THD+N is (R) by default and THD is (F) by default.
    
    Keep `thd_analyzer()` so that `thd_analyzer_launcher.py` works, too.
    
    Verified that both THD+N(R) and THD(F), in both percent and dB, match my
    XLS calculator:
    
    ```shell
    λ python scripts\thd_analyzer.py "whine noise test -10 -20 -15 -25 -30 -20 -35 dBFS.wav"
    Analyzing "whine noise test -10 -20 -15 -25 -30 -20 -35 dBFS.wav"...
    THD+N(R):       59.9492% or -4.4 dB
    THD(F):         74.9008% or -2.5 dB
    ```
    
    And these match the claimed values as well:
    
    ```
    File                        Claimed
    Reference           (440Hz) 0.0%
    Single-ended triode (440SE) 5.0%
    Solid state         (440SS) 0.5%
    ```
    
    ```shell
    λ python scripts\thd_analyzer.py tests\thd_files\440Hz.wav
    Analyzing "tests\thd_files\440Hz.wav"...
    -- Left and Right channels are identical --
    THD+N(R):       0.0022% or -93.3 dB
    THD(F):         0.0013% or -98.0 dB
    
    λ python scripts\thd_analyzer.py tests\thd_files\440SE.wav
    Analyzing "tests\thd_files\440SE.wav"...
    -- Left channel --
    THD+N(R):       5.0649% or -25.9 dB
    THD(F):         5.0714% or -25.9 dB
    -- Right channel --
    THD+N(R):       5.0648% or -25.9 dB
    THD(F):         5.0713% or -25.9 dB
    
    λ python scripts\thd_analyzer.py tests\thd_files\440SS.wav
    Analyzing "tests\thd_files\440SS.wav"...
    -- Left channel --
    THD+N(R):       0.5124% or -45.8 dB
    THD(F):         0.5062% or -45.9 dB
    -- Right channel --
    THD+N(R):       0.5124% or -45.8 dB
    THD(F):         0.5061% or -45.9 dB
    ```

--- scripts/thd_analyzer.py
+++ scripts/thd_analyzer.py
@@ -1,46 +1,49 @@
+from math import log10
+from time import time
+
 from waveform_analysis._common import analyze_channels
-from waveform_analysis.thd import THDN
-
-# if __name__ == '__main__':
-#
-#
-#    try:
-#
-#        if files:
-#            for filename in files:
-#                try:
-#                    analyze_channels(filename, THDN)
-#                except IOError:
-#                    print('Couldn\'t analyze "' + filename + '"\n')
-#                print('')
-#        else:
-#            sys.exit("You must provide at least one file to analyze")
-#    except BaseException as e:
-#        print('Error:')
-#        print(e)
-#        raise
-#    finally:
-#        # Otherwise Windows closes the window too quickly to read
-#        input('(Press <Enter> to close)')
-#
+from waveform_analysis.thd import THD, THDN
 
 
-# print(f'Frequency: {fs * (true_i / len(windowed)):f} Hz')
+def thd_wrapper(signal, fs):
+    """Wrapper function to analyze THD+N and THD, then print results"""
+    # Calculate THD+N
+    thdn = THDN(signal, fs)
+    # Calculate THD
+    thd = THD(signal, fs)
 
-# print(f"THD+N:      {THDN * 100:.4f}% or {20 * log10(THDN):.1f} dB")
-# print(f"A-weighted: {THDNA * 100:.4f}% or {20 * log10(THDNA):.1f} dB(A)")
+    # Print results in both % and dB
+    print(f"THD+N(R):\t{thdn * 100:.4f}% or {20 * log10(thdn):.1f} dB")
+    print(f"THD(F):  \t{thd * 100:.4f}% or {20 * log10(thd):.1f} dB")
 
 
 def thd_analyzer(files):
-    import sys
-
+    """Function for the launcher script to call"""
     if files:
         for filename in files:
             try:
-                analyze_channels(filename, THDN)
+                start_time = time()
+                analyze_channels(filename, thd_wrapper)
+                print(f'\nTime elapsed: {time() - start_time:.3f} s\n')
             except IOError:
                 print(f"Couldn't analyze \"{filename}\"\n")
             print('')
     else:
         sys.exit("You must provide at least one file to analyze:\n"
-                 "python wave_analyzer.py filename.wav")
+                 "python thd_analyzer.py filename.wav")
+
+
+if __name__ == '__main__':
+    import sys
+    try:
+        files = sys.argv[1:]
+        thd_analyzer(files)
+    except BaseException as e:
+        print('Error:')
+        print(e)
+        raise
+    finally:
+        # Only wait for input when running in an interactive console
+        # Otherwise Windows closes the window too quickly to read
+        if sys.stdout.isatty():
+            input('(Press <Enter> to close)')
